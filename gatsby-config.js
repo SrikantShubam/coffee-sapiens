641952719b0762fad2f99714b6c9ee2b55bae5a2
SrikantShubam <srikantshubham.personal@gmail.com>
2024-09-27 12:27:01 +0530

    added in google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,6 +40,17 @@ module.exports = {
         icon: `src/images/logo.jpg`, // Path to your favicon file
       },
     },
+    {
+      resolve: `gatsby-plugin-gtag`,
+      options: {
+        // your google analytics tracking id
+        trackingId: `G-3XPL2YZPWE`,
+        // Puts tracking script in the head instead of the body
+        head: true,
+        // enable ip anonymization
+        anonymize: true,
+      },
+    },
    
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
